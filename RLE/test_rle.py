823003ca6d3a3cf2802a92ed7bd5f7925f774938
Anders Lehmann <redorlik@gmail.com>
2017-09-04 09:47:46 +0200

    Delete premature tests

--- RLE/test_rle.py
+++ RLE/test_rle.py
@@ -2,10 +2,3 @@ from RLE import encode
 
 def test_encode():
     assert encode('kkkkkbbbb') == '5k4b'
-
-def test_encode_empty():
-    assert encode('') == ''
-
-def test_encode_emoji():
-    assert encode('😇') == '1😇'
-    assert encode('😇😇😇😇😇') == '5😇'
